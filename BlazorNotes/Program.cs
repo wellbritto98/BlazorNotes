b500e27f0f239e33c98a1f6f159736d027198edf
José Wellington <josewellington@infowsoftware.com.br>
2024-02-15 18:26:00 -0300

    Comingback

--- BlazorNotes/Program.cs
+++ BlazorNotes/Program.cs
@@ -1,11 +1,14 @@
+using Blazored.Modal;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorNotes;
 
+string credentialFirebasePath = "BlazorNotes/Properties/blazor-notes-firebase-adminsdk-y20l2-58720ec7b5.json";
+Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialFirebasePath);
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddBlazoredModal();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
